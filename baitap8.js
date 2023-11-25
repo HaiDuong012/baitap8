7ea977efb8b9e297933d195d31680a6fe41b37f9
Duong Hai <haibich012@gmail.com>
2023-11-25 20:33:21 +0700

    baitap8

---
+++ baitap8.js
@@ -0,0 +1,139 @@
+//bai tập tính số cân nặng cuả cơ thể:
+
+// let height = +prompt("mời nhập chiều cao");
+// let weight = +prompt("mời nhập cân nặng");
+
+// let bmi = weight/(height);
+// if (bmi < 18.5){
+//     console.log("cân nặng thấp") ;
+// }else if(bmi>=18.5 && bmi<24.9){
+//     console.log("cân nặng bình thường");
+// }else if (bmi>= 25 ){
+//     console.log("Thừa cân");
+// }else if (bmi > 25 && bmi<= 29){
+//     console.log("Tiền béo phì");
+// }
+// else if (bmi > 30 && bmi<= 34.9){
+//     console.log("béo phì độ 1");
+// }
+// else if (bmi > 35 && bmi<= 39.9){
+//     console.log("béo phì độ 2");
+// }
+// else {
+//     console.log("béo phì độ 3");
+// }
+
+//bài tập câu lệnh switch -case
+// let month = +prompt("mời nhập số tháng");
+// switch (month) {
+// case 1:
+//     console.log("Tháng này có 30 ngày")
+//     break;
+// case 2:
+//     console.log("Tháng này có 31 ngày")
+//     break;
+// case 3:
+//     console.log("Tháng này có 31 ngày")
+//     break;
+// case 4:
+//     console.log("Tháng này có 29 ngày")
+//     break;
+// case 5 :
+//     console.log("Tháng này có 28 ngày")
+//     break;
+// case 6:
+//     console.log("Tháng này có 30 ngày")
+//     break;
+// case 7:
+//     console.log("Tháng này có 19 ngày")
+//     break;
+// case 8:
+//     console.log("Tháng này có 35 ngày")
+//     break;
+// case 9:
+//     console.log("Tháng này có 36 ngày")
+//     break;
+// case 10:
+//     console.log("Tháng này có 33 ngày")
+//     break;
+// case 11 :
+//     console.log("Tháng này có 31 ngày")
+//     break;
+// case 12 :
+//     console.log("Tháng này có 31 ngày")
+//     break;
+
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
+//         console.log("31 ngay");
+//         break;
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//         console.log("30 ngay");
+//         break;
+//     case 2:
+//         console.log("28 ngay");
+//         break;
+//     default:
+//         console.log(null);
+//         break;
+// }
+
+//may tinh don gian
+
+// let a = +prompt("mời nhập số a");
+// let b= +prompt("mời nhập số b");
+// let c= prompt("Nhap c");
+// // let d = acb;
+
+// switch (c) {
+//     case "+":
+//         alert(a + b);
+//         break;
+
+//     case "-":
+//         alert(a-b);
+//         break;
+//     case "*":
+//         alert(a*b);
+//         break;
+//     case "/":
+//         alert(a/b);
+//         break;
+//     default:
+//         alert(NaN);
+//         break;
+// }
+
+//tim min-max
+// let a = 6;
+// let b = 1;
+// let c=  5;
+// if(a>b && b>c){
+//     console.log("max:", a);
+//     console.log("min:",c);
+// }else if(b>a && a>c){
+//     console.log("max:",b);
+//     console.log("min:",c)
+// }else if(c>a && a>b){
+//     console.log("max:",c);
+//     console.log("min:",b);
+// }else if(a>c && c>b ){
+//     console.log("max:",a);
+//     console.log("min:",b);
+// }else if(b>c && c>a ){
+//     console.log("max:",b);
+//     console.log("min:",a);
+// }else if (c>b && b>a){
+//     console.log("max:",c);
+//     console.log("min:",a);
+// }else{
+//     console.log("max,min:", undefined);
+// }
